f1316351cb58424b419e1a15edeeddb166c15643
Harsh Sharma <harsh@codelif.in>
2025-01-22 19:30:05 +0530

    test

--- generate_cache.py
+++ generate_cache.py
@@ -100,7 +100,7 @@ def filter_events(evs: List[Event], batch: str, day: str) -> List[dict]:
 
         data["subject"] = ev.event or ev.eventcode
         data["subjectcode"] = ev.eventcode
-        data["teacher"] = ', '.join(ev.lecturer)
+        data["teacher"] = ', '.join(ev.lecturer) + 'wow'
         data["batches"] = ev.batches
         data["venue"] = ev.classroom
         data["type"] = ev.event_type
